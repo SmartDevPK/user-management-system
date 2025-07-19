abd97b1218a2f79dae1a00528e5b89603282ec3f
michael emmanuel <emmanuelmichaelpk3@gmail.com>
2025-07-19 03:34:48 +0100

    Add User Route to the codebase

--- app.js
+++ app.js
@@ -1,24 +1,42 @@
-const express = require ('express');
-const bodyParser = require ('body-parser');
-const app = express ()
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
-const PORT = 3000
+const PORT = 3000;
 
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
 
 // Middleware
-app.set('view engine', 'ejs');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
+// Sample User Data
+let users = [
+    { userUniqueId: "1", userName: "Emmanuel Michael", userEmail: "emmanuelmichaelpk3@gmail", userAge: "27" },
+    { userUniqueId: "2", userName: "Hypertek", userEmail: "emmanuel@gmail.com", userAge: "24" },
+    { userUniqueId: "3", userName: "Hypertek", userEmail: "emmanuelmichael@gmail.com", userAge: "24" }
+];
 
-app.get('/',(req, res)=>{
-    res.render("home", ({data:users}));
-})
+// Home Route - Display users
+app.get('/', (req, res) => {
+    res.render("home", { data: users });
+});
 
+// Add User Route
+app.post('/', (req, res) => {
+    const newUser = {
+        userUniqueId: req.body.userUniqueId,
+        userName: req.body.userName,
+        userEmail: req.body.userEmail,
+        userAge: req.body.userAge
+    };
 
+    users.push(newUser);
+    res.render("home", { data: users });
+});
 
-
-
-app.listen(PORT, ()=>{
-    console.log(`Server running on http://localhost:${PORT}`)
-})
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
